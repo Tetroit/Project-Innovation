d1ade382083d1c6712f3d26916eecec317a4baa0
Tetroit <shichinov.ivan@gmail.com>
2025-02-25 22:50:18 +0100

    sensor info debug

--- Assets/SensorInput.cs
+++ Assets/SensorInput.cs
@@ -5,6 +5,14 @@ using UnityEngine.InputSystem;
 using UnityEngine;
 using UnityEditor;
 using System;
+using UnityEngine.UIElements;
+using UnityEngine.InputSystem.Controls;
+using System.Linq;
+using System.IO;
+
+
+
+
 
 
 
@@ -21,8 +29,39 @@ using Gyroscope = UnityEngine.InputSystem.Android.AndroidGyroscope;
 using LightSensor = UnityEngine.InputSystem.Android.AndroidLightSensor;
 #endif
 
+public interface IControlInfo
+{
+    public string name { get; }
+    
+}
+public struct ControlInfo<T> : IControlInfo where T : struct
+{
+    public string name;
+    public InputControl<T> control;
+    public T value;
+    string IControlInfo.name => name;
+
+    public ControlInfo (string name, InputControl<T> control)
+    {
+        this.name = name;
+        this.control = control;
+        value = control.ReadValue();
+    }
+}
+
+public struct ControlToDisplay
+{
+    public string name;
+    public TextMeshProUGUI display;
+}
+
 public class SensorInput : MonoBehaviour
 {
+    [SerializeField]
+    List<ControlToDisplay> controlsToDisplays;
+    public Dictionary<string, TextMeshProUGUI> dataLayout => 
+        controlsToDisplays.ToDictionary(field => field.name, field => field.display);
+
     [SerializeField]
     TextMeshProUGUI accelerometerInfo;
     [SerializeField]
@@ -32,20 +71,229 @@ public class SensorInput : MonoBehaviour
     [SerializeField]
     TextMeshProUGUI sensorInfo;
 
-    //INPUT:
-    // Accelerometer: OK
-    // Gyroscope: OK
-    // LightSensor: NO SUPPORT
-    //INPUT SYSTEM:
-    // Accelerometer: OK
-    // Gyroscope: FAIL
-    // LightSensor: NO IMPLEMENTATION
-    //
 
     bool accelerometerFound = false;
     bool gyroscopeFound = false;
     bool lightSensorFound = false;
-    // Start is called before the first frame update
+
+    Vector3 acceleration;
+    Vector3 angularVelocity;
+    float lightLevel;
+
+    [SerializeField]
+    float sensorDetectionDelay = 0.01f; 
+
+    int currentDeviceId = 0;
+
+    private void OnEnable()
+    {
+        Invoke(nameof(InputSystemFindDevices), sensorDetectionDelay);
+#if !UNITY_EDITOR
+        sensorInfo.text = 
+        Application.dataPath + "\n" + 
+        Application.persistentDataPath;
+#endif
+    }
+
+    void InputSystemFindDevices()
+    {
+#if UNITY_EDITOR
+        foreach (var dev in InputSystem.devices)
+        {
+            if (dev.layout == "Accelerometer")
+                accelerometerFound = true;
+
+            if (dev.layout == "Gyroscope")
+                gyroscopeFound = true;
+        }
+
+        Invoke(nameof(InputSystemInitAccelerometer), Time.deltaTime);
+        Invoke(nameof(InputSystemInitGyroscope), Time.deltaTime);
+
+
+#else
+        foreach (var dev in InputSystem.devices)
+        {
+            if (dev.layout == "AndroidAccelerometer")
+                accelerometerFound = true;
+
+            if (dev.layout == "AndroidGyroscope")
+                gyroscopeFound = true;
+
+            if (dev.layout == "AndroidLightSensor")
+                lightSensorFound = true;
+        }
+        
+        Invoke(nameof(InputSystemInitAccelerometer), Time.deltaTime);
+        Invoke(nameof(InputSystemInitGyroscope), Time.deltaTime);
+        Invoke(nameof(InputSystemInitLightSensor), Time.deltaTime);
+
+        foreach (InputDevice device in InputSystem.devices)
+        {
+            string sensors = "";
+            foreach (InputControl ic in device.children)
+            {
+                WriteChildrenControls(ic, ref sensors, 1);
+            }
+            sensors += '\n';
+
+            string path = Application.persistentDataPath + "/" + device.layout + ".txt";
+            File.WriteAllText(path, sensors);
+
+            sensorInfo.text = path;
+
+        }
+
+#endif
+        if (!gyroscopeFound && gyroscopeInfo != null)
+            gyroscopeInfo.text = "Gyroscope: not found";
+        if (!accelerometerFound)
+            accelerometerInfo.text = "Accelerometer: not found";
+        if (!lightSensorFound)
+            lightSensorInfo.text = "LightSensor: not found";
+    }
+    void Update()
+    {
+        InputSystemAccelerometer();
+
+        InputSystemGyroscope();
+
+#if UNITY_EDITOR
+        DisplaySensorInfo();
+#else
+        InputSystemLightSensor();
+#endif
+    }
+
+    void DisplaySensorInfo()
+    {
+        if (sensorInfo == null) return;
+
+        //listing all sensors
+        string sensors = "";
+
+        InputSystem.EnableDevice(Keyboard.current);
+
+        if (Keyboard.current.aKey.wasPressedThisFrame)
+        {
+            Debug.Log("aKey");
+            currentDeviceId--;
+            if (currentDeviceId < 0)
+                currentDeviceId = InputSystem.devices.Count - 1;
+        }
+        if (Keyboard.current.dKey.wasPressedThisFrame)
+        {
+            Debug.Log("dKey");
+            currentDeviceId++;
+            if (currentDeviceId >= InputSystem.devices.Count)
+                currentDeviceId = 0;
+        }
+
+        var device = InputSystem.devices[currentDeviceId];
+
+        sensors += device.deviceId + "  ";
+        sensors += device.layout + ":  \n";
+        sensors += (device.enabled ? "ENABLED" : "DISABLED") + "\n\n";
+
+        foreach (InputControl ic in device.children)
+        {
+            WriteChildrenControls(ic, ref sensors, 1);
+        }
+
+        if (Keyboard.current.sKey.wasPressedThisFrame)
+        {
+            string path = Application.dataPath + "/device info";
+
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            path += "/" + device.layout + ".txt";
+            File.WriteAllText(path, sensors);
+            Debug.Log("Log saved to: " + path);
+        }
+        sensors += '\n';
+
+        sensorInfo.text = sensors;
+    }
+
+    void WriteChildrenControls(InputControl control, ref string result, int it)
+    {
+        for (int i = 0; i < it; i++)
+            result += "\t";
+
+        result += "<" + control.valueType + "> " + control.displayName;
+
+        if (control.children.Count == 0)
+        {
+            result += " = " + control.ReadValueAsObject() + '\n';
+            return;
+        }
+
+        result += '\n';
+
+        foreach (var child in control.children)
+        {
+            WriteChildrenControls(child, ref result, it + 1);
+        }
+        return;
+    }
+    void InputSystemInitAccelerometer()
+    {
+        if (!accelerometerFound) return;
+        InputSystem.EnableDevice(Accelerometer.current);
+    }
+
+    void InputSystemAccelerometer()
+    {
+        if (!accelerometerFound) return;
+
+        Debug.Log("Accelerometer: " + Accelerometer.current.enabled);
+
+        if (Accelerometer.current.enabled)
+            acceleration = Accelerometer.current.acceleration.ReadValue();
+
+        if (accelerometerInfo != null)
+            accelerometerInfo.text = "Accelerometer: " + acceleration;
+    }
+
+    void InputSystemInitGyroscope()
+    {
+        if (!gyroscopeFound) return;
+        InputSystem.EnableDevice(Gyroscope.current);
+    }
+    void InputSystemGyroscope()
+    {
+        if (!gyroscopeFound) return;
+
+        InputSystem.EnableDevice(Gyroscope.current);
+        Debug.Log("Gyroscope: " + Gyroscope.current.enabled);
+
+        if (Gyroscope.current.enabled)
+            angularVelocity = Gyroscope.current.angularVelocity.ReadValue();
+
+        if (gyroscopeInfo != null)
+            gyroscopeInfo.text = "Gyroscope: " + Gyroscope.current.angularVelocity.ReadValue();
+    }
+    void InputSystemInitLightSensor()
+    {
+        if (!lightSensorFound) return;
+        InputSystem.EnableDevice(LightSensor.current);
+    }
+    void InputSystemLightSensor()
+    {
+        if (!lightSensorFound) return;
+
+        InputSystem.EnableDevice(LightSensor.current);
+        Debug.Log("LightSensor: " + LightSensor.current.enabled);
+
+        if (LightSensor.current.enabled)
+            lightLevel = LightSensor.current.lightLevel.ReadValue();
+
+        if (lightSensorInfo != null)
+            lightSensorInfo.text = "LightSensor: " + LightSensor.current.lightLevel.ReadValue();
+    }
+
+
+
 
     [Obsolete("Use input system instead of input")]
     void InputInitGyroscope()
@@ -68,124 +316,5 @@ public class SensorInput : MonoBehaviour
         Debug.Log("Gyroscope: " + Input.gyro.enabled);
 
         accelerometerInfo.text = "Accelerometer: " + Input.acceleration;
-    }
-    void Start()
-    {
-    }
-    private void OnEnable()
-    {
-        Invoke(nameof(InputSystemFindDevices),0.01f);
-
-        Invoke(nameof(InputSystemInitAccelerometer), 0.02f);
-        Invoke(nameof(InputSystemInitGyroscope), 0.02f);
-
-#if !UNITY_EDITOR
-        if (lightSensorFound)
-            Invoke(nameof(InputSystemInitLightSensor), 0.02f);
-#endif
-    }
-
-    void InputSystemFindDevices()
-    {
-#if UNITY_EDITOR
-        foreach (var dev in InputSystem.devices)
-        {
-            if (dev.layout == "Accelerometer")
-                accelerometerFound = true;
-
-            if (dev.layout == "Gyroscope")
-                gyroscopeFound = true;
-        }
-#else
-        foreach (var dev in InputSystem.devices)
-        {
-            if (dev.layout == "AndroidAccelerometer")
-                accelerometerFound = true;
-
-            if (dev.layout == "AndroidGyroscope")
-                gyroscopeFound = true;
-
-            if (dev.layout == "AndroidLightSensor")
-                lightSensorFound = true;
-        }
-#endif
-
-        if (!gyroscopeFound)
-            gyroscopeInfo.text = "Gyroscope: not found";
-        if (!accelerometerFound)
-            accelerometerInfo.text = "Accelerometer: not found";
-        if (!lightSensorFound)
-            lightSensorInfo.text = "LightSensor: not found";
-    }
-    void Update()
-    {
-            InputSystemAccelerometer();
-
-            InputSystemGyroscope();
-
-#if !UNITY_EDITOR
-        InputSystemLightSensor();
-#endif
-        //listing all sensors
-        string sensors = "";
-        foreach (InputDevice device in InputSystem.devices)
-        {
-            sensors += device.deviceId + "|";
-            sensors += device.GetType() + "|";
-            sensors += device.layout + "|";
-            sensors += device.name + "|";
-            sensors += '\n';
-        }
-
-        sensorInfo.text = sensors;
-    }
-
-    void InputSystemInitAccelerometer()
-    {
-        InputSystem.EnableDevice(Accelerometer.current);
-    }
-
-    void InputSystemAccelerometer()
-    {
-        if (!accelerometerFound) return;
-
-        Debug.Log("Accelerometer: " + Accelerometer.current.enabled);
-
-        if (Accelerometer.current.enabled)
-            accelerometerInfo.text = "Accelerometer: " + Accelerometer.current.acceleration.ReadValue();
-    }
-
-    void InputSystemInitGyroscope()
-    {
-        InputSystem.EnableDevice(Gyroscope.current);
-    }
-    void InputSystemGyroscope()
-    {
-        if (!gyroscopeFound) return;
-
-        InputSystem.EnableDevice(Gyroscope.current);
-        Debug.Log("Gyroscope: " + Gyroscope.current.enabled);
-
-        if (Gyroscope.current.enabled)
-            gyroscopeInfo.text = "Gyroscope: " + Gyroscope.current.angularVelocity.ReadValue();
-    }
-
-    public void SwitchInputMethod()
-    {
-        //TO BE IMPLEMENTED
-    }
-    void InputSystemInitLightSensor()
-    {
-        InputSystem.EnableDevice(LightSensor.current);
-    }
-    void InputSystemLightSensor()
-    {
-        if (!lightSensorFound) return;
-
-        InputSystem.EnableDevice(LightSensor.current);
-        Debug.Log("LightSensor: " + LightSensor.current.enabled);
-
-        if (LightSensor.current.enabled)
-            lightSensorInfo.text = "LightSensor: " + LightSensor.current.lightLevel.ReadValue();
     }
 }
