057a234e97749fd2836f30bcf34f753d8f68f776
KrijnSlot <krijnslot112233@gmail.com>
2025-03-10 13:29:12 +0100

    orrery puzzle rotating + check

---
+++ Assets/Scripts/Puzzles/OrreryPuzzle2.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrreryPuzzle2 : Puzzle
+{
+    private readonly float[] targetRotations = { -51.644f, 108.605f, -136.443f, 13.939f, -14.305f, 165.561f, -105.264f, 75.796f };
+    private const float tolerance = 10f;
+
+    protected override void Setup()
+    {
+
+    }
+
+    public override bool CheckCondition()
+    {
+        for (int i = 0; i < elements.Count; i++)
+        {
+            float rotY = NormalizeAngle(elements[i].transform.rotation.eulerAngles.y);
+
+            if (Mathf.Abs(rotY - targetRotations[i]) >= tolerance)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private float NormalizeAngle(float angle)
+    {
+        return (angle > 180f) ? angle - 360f : angle;
+    }
+}
